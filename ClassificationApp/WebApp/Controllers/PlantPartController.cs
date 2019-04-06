8faa9684eb7ac155f82f15892e30e0699da0a659
mpalmeos <maie.palmeos@itcollege.ee>
2019-04-06 18:45:36 +0300

    Commit before everything is broken.

--- ClassificationApp/WebApp/Controllers/PlantPartController.cs
+++ ClassificationApp/WebApp/Controllers/PlantPartController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DAL;
+using DAL.App.EF;
 using Domain;
 
 namespace WebApp.Controllers
--- WebClient/src/main-router.ts
+++ WebClient/src/main-router.ts
@@ -16,12 +16,23 @@ export class MainRouter {
     log.debug('configureRouter running');
     
     this.router = router;
-    config.title = 'Aurelia';
+    config.title = 'WebClient - Aurelia';
     config.map([
       {route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home'), nav: true, title: 'Home'},
       
+      //{route: '', name: '', moduleId: PLATFORM.moduleName(''), nav: true, title: ''},
       {route: ['companies', 'companies/index'], name: 'companies' + 'Index', moduleId: PLATFORM.moduleName('companies/index'), nav: true, title: 'Companies'},
       {route: 'companies/create', name: 'companies' + 'Create', moduleId: PLATFORM.moduleName('companies/create'), nav: true},
+      
+      /*
+      {route: ['persons','persons/index'], name: 'persons' + 'Index', moduleId: PLATFORM.moduleName('persons/index'), nav: true, title: 'Persons'},
+        {route: 'persons/create', name: 'persons' + 'Create', moduleId: PLATFORM.moduleName('persons/create'), nav: false, title: 'Persons Create'},
+        {route: 'persons/edit/:id', name: 'persons' + 'Edit', moduleId: PLATFORM.moduleName('persons/edit'), nav: false, title: 'Persons Edit'},
+        {route: 'persons/delete/:id', name: 'persons' + 'Delete', moduleId: PLATFORM.moduleName('persons/delete'), nav: false, title: 'Persons Delete'},
+        {route: 'persons/details/:id', name: 'persons' + 'Details', moduleId: PLATFORM.moduleName('persons/details'), nav: false, title: 'Persons Details'},
+      */ 
+      
+      
     ]);
   }
   
