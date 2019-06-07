86398173b6ddb9080f4b54b1b54e7dc7c121a6eb
mpalmeos <maie.palmeos@itcollege.ee>
2019-06-07 09:27:11 +0300

    Forgot password and username in front-end.

--- WebClient/src/identity/login.ts
+++ WebClient/src/identity/login.ts
@@ -8,8 +8,10 @@ export var log = LogManager.getLogger("app.Login.Index");
 @autoinject
 export class Login {
 
-  private email: string = "a@a.ee";
-  private password: string = "Password";
+  private email: string;
+  private password: string;
+  //private email: string = "a@a.ee";
+  //private password: string = "Password";
 
   constructor(
     private identityService: IdentityService,
